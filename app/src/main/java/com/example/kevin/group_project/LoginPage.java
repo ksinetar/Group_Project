4c0fd3374ec9f6d8c76da3f7b3c94acc813c297b
movethe2chains <gaykevinjames@gmail.com>
2017-11-07 17:50:20 -0500

    test2

--- app/src/main/java/com/example/kevin/group_project/LoginPage.java
+++ app/src/main/java/com/example/kevin/group_project/LoginPage.java
@@ -8,6 +8,6 @@ public class LoginPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_login_page2);
     }
 }
