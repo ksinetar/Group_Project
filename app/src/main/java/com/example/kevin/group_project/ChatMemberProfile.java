9c6a0c4f5b059b2ce9f69981417401c5e6513adf
movethe2chains <gaykevinjames@gmail.com>
2017-11-07 18:04:05 -0500

    test2

---
+++ app/src/main/java/com/example/kevin/group_project/ChatMemberProfile.java
@@ -0,0 +1,13 @@
+package com.example.kevin.group_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatMemberProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_member_profile);
+    }
+}
