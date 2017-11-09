146f55391c07695c45d61bf6908913998fb0c20e
movethe2chains <gaykevinjames@gmail.com>
2017-11-09 13:36:24 -0500

    test2

--- app/src/main/java/com/example/kevin/group_project/SignUp.java
+++ app/src/main/java/com/example/kevin/group_project/SignUp.java
@@ -2,12 +2,16 @@ package com.example.kevin.group_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SignUp extends Activity {
+
+    private TextView textViewSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        textViewSignUp = findViewById(R.id.textViewSignUp);
     }
 }
