6c36c57f06e57ff5d25fd71aa2022e622e2988a6
Austin <ausash@umich.edu>
2017-11-10 17:47:10 -0500

    Sign Up Pages Updates

--- app/src/main/java/com/example/kevin/group_project/SignUp2.java
+++ app/src/main/java/com/example/kevin/group_project/SignUp2.java
@@ -2,11 +2,15 @@ package com.example.kevin.group_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SignUp2 extends Activity implements Button.OnClickListener {
 
@@ -31,6 +35,7 @@ public class SignUp2 extends Activity implements Button.OnClickListener {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Gender));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerGender.setAdapter(myAdapter);
+
     }
 
     @Override
