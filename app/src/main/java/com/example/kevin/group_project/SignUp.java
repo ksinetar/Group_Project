d60b164ac894413bf7f9873047a071acfa8a169a
Austin <ausash@umich.edu>
2017-11-10 15:56:44 -0500

    Build.Gradle Fix

--- app/src/main/java/com/example/kevin/group_project/SignUp.java
+++ app/src/main/java/com/example/kevin/group_project/SignUp.java
@@ -3,15 +3,19 @@ package com.example.kevin.group_project;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUp extends Activity {
+public class SignUp extends Activity implements Button.OnClickListener {
 
-    private EditText editTextUsername, editTextName, editTextEmail, editTextPassword, editTextUniversity, editTextCity, editTextDateofbirth;
+    private EditText editTextUsername, editTextName, editTextEmail;
+    private EditText editTextPassword, editTextUniversity, editTextCity;
+    private EditText editTextDateofbirth;
 
-
-
+    private Button buttonSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,39 @@ public class SignUp extends Activity {
         editTextCity = findViewById(R.id.editTextCity);
         editTextDateofbirth = findViewById(R.id.editTextDateofbirth);
 
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        buttonSignUp.setOnClickListener(this);
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonSignUp:
+
+                if ((editTextUsername.getText().toString().isEmpty())) {
+                    Toast.makeText(SignUp.this, "Please Fill All Fields",
+                            Toast.LENGTH_SHORT).show();
+                } else if (editTextPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(SignUp.this, "Please Fill All Fields",
+                            Toast.LENGTH_SHORT).show();
+                } else if (editTextName.getText().toString().isEmpty()) {
+                    Toast.makeText(SignUp.this, "Please Fill All Fields",
+                            Toast.LENGTH_SHORT).show();
+                } else if (editTextEmail.getText().toString().isEmpty()) {
+                    Toast.makeText(SignUp.this, "Please Fill All Fields",
+                            Toast.LENGTH_SHORT).show();
+                } else if (editTextUniversity.getText().toString().isEmpty()) {
+                    Toast.makeText(SignUp.this, "Please Fill All Fields",
+                            Toast.LENGTH_SHORT).show();
+                } else if (editTextCity.getText().toString().isEmpty()) {
+                    Toast.makeText(SignUp.this, "Please Fill All Fields",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    //Add code for adding provided info to Firebase, then take user to homepage
+                }
+        }
     }
 }
