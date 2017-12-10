4aa0ac30a4f4b8505e1bb97a1d641e36c916c999
jamesmiller <jamestmiller@icloud.com>
2017-12-09 21:24:13 -0500

    adds user to group director

--- app/src/main/java/com/example/kevin/group_project/SearchGroupInfo.java
+++ app/src/main/java/com/example/kevin/group_project/SearchGroupInfo.java
@@ -90,7 +90,7 @@ public class SearchGroupInfo extends Activity implements Button.OnClickListener 
             public void onDataChange(DataSnapshot dataSnapshot) {
             foundName = dataSnapshot.child("fullname").getValue().toString();
                 Toast.makeText(SearchGroupInfo.this, foundName, Toast.LENGTH_SHORT).show();
-                mFirebaseDatabase.child("groups").child(groupname).child("users").setValue(foundName);
+                mFirebaseDatabase.child("groups").child(groupname).child("users").child("name").setValue(foundName);
             }
 
             @Override
